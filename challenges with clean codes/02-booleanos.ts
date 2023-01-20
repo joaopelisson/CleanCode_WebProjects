14d4e5a7ea39f3af5f105381fdfc3570ac98a60d
joao.pelisson <joaopedro.devbr@gmail.com>
2023-01-19 22:33:22 -0300

    second challenge - boolean naming

---
+++ challenges with clean codes/02-booleanos.ts
@@ -0,0 +1,31 @@
+// Boleanos
+
+const AboutUser = {
+  name: 'Diego Fernandes',
+  height: 190,
+  hasTicket: true,
+}
+
+const MinimumHeightRequired = 130;
+
+const currentHour = new Date().getHours()
+
+const isTheParkOpen = currentHour > 9 && currentHour < 18
+
+if (!isTheParkOpen) {
+  throw new Error('O parque está fechado!')
+}
+
+const TheUserHasATicket = AboutUser.hasTicket
+
+if (!TheUserHasATicket) {
+  throw new Error('O Diego não possui um bilhete para entrar no parque!')
+}
+
+const heCanGetIntoTheToy = AboutUser.height > MinimumHeightRequired
+
+if (!heCanGetIntoTheToy) {
+  throw new Error('O Diego não pode entrar no brinquedo!')
+} 
+
+console.log('O Diego se divertiu muito! :)')
